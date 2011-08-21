ce443402cc28b0e345628696d636fd91463ab103
Dan Hagon <axiomsofchoice@gmail.com>
2011-08-21 20:45:12 +0100

    Provided more detailed documentation of the OAI-PMH error code excepions.

--- src/oaipmh/error.py
+++ src/oaipmh/error.py
@@ -1,3 +1,11 @@
+"""Exceptions based on OAI-PMH v2.0 error codes.
+
+The full list of error codes can be found in section 3.6 of The Open Archives 
+Initiative Protocol for Metadata Harvesting specification, protocol version
+2.0.
+
+http://www.openarchives.org/OAI/openarchivesprotocol.html#ErrorConditions
+"""
 
 class ErrorBase(Exception):
     def oainame(self):
@@ -9,34 +17,59 @@ class ErrorBase(Exception):
         return name
 
 class BadArgumentError(ErrorBase):
-    pass
-
-class BadVerbError(ErrorBase):
+    """The request includes illegal arguments, is missing required arguments,
+    includes a repeated argument, or values for arguments have an illegal
+    syntax.
+    """
     pass
 
 class BadResumptionTokenError(ErrorBase):
+    """The value of the resumptionToken argument is invalid or expired.
+    """
     pass
 
+class BadVerbError(ErrorBase):
+    """Value of the verb argument is not a legal OAI-PMH verb, the verb
+    argument is missing, or the verb argument is repeated.
+    """
+    pass
+
+
 class CannotDisseminateFormatError(ErrorBase):
+    """The metadata format identified by the value given for the metadataPrefix
+    argument is not supported by the item or by the repository.
+    """
     pass
 
 class IdDoesNotExistError(ErrorBase):
+    """The value of the identifier argument is unknown or illegal in this
+    repository.
+    """
     pass
 
 class NoRecordsMatchError(ErrorBase):
+    """The combination of the values of the from, until, set and metadataPrefix
+    arguments results in an empty list.
+    """
     pass
 
 class NoMetadataFormatsError(ErrorBase):
+    """There are no metadata formats available for the specified item.
+    """
     pass
 
 class NoSetHierarchyError(ErrorBase):
+    """The repository does not support sets.
+    """
     pass
 
 class UnknownError(ErrorBase):
+    """An unkown OAI-PMH error has occurred.
+    """
     pass
 
-# errors not defined by OAI-PMH but which can occur in a client when
-# the server is somehow misbehaving
+# Errors not defined by OAI-PMH but which can occur in a client when the server
+# is somehow misbehaving
 class ClientError(Exception):
     def details(self):
         """Error details in human readable text.
